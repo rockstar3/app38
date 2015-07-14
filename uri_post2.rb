94674f047bcffc571b2032279116d1ae17d2ef79
root <kolja.schneider1125@gmail.com>
2015-07-14 07:53:34 +0200

    uri open

--- uri_post2.rb
+++ uri_post2.rb
@@ -5,15 +5,15 @@ require 'net/http'
 
 uri = URI.parse('http://us.topshop.com/webapp/wcs/stores/servlet/CatalogNavigationSearchResultCmd?Nrpp=20&No=20&pageNum=2&beginIndex=1&endIndex=11&pageFlag=s_20&N=14313+17446&catId=208634&parent_categoryId=208580')
 
-# resp, data = Net::HTTP.post_form(url, params)
-# puts resp.inspect
-# puts data.inspect
+resp, data = Net::HTTP.post_form(url, params)
+puts resp.inspect
+puts data.inspect
 
-# req = Net::HTTP::Post.new("#{uri.path}?#{uri.query}", {
-#     'Referer' => "http://www.example.com/referer",
-#     'User-Agent'=> "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
-#     'Cookie' => $cookie
-# })
+req = Net::HTTP::Post.new("#{uri.path}?#{uri.query}", {
+    'Referer' => "http://www.example.com/referer",
+    'User-Agent'=> "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+    'Cookie' => $cookie
+})
 http = Net::HTTP.new(uri.host, uri.port)
 req = Net::HTTP::Post.new("#{uri.path}?#{uri.query}", 
 	{'User-Agent'=> "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)", 'Content-Type' =>'text/html; charset=UTF-8'})
