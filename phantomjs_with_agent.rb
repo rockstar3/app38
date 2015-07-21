fdf3ee6a70456187bbdb380a13f4193d6d59f4ed
root <kolja.schneider1125@gmail.com>
2015-07-21 08:44:11 +0200

    fix

--- phantomjs_with_agent.rb
+++ phantomjs_with_agent.rb
@@ -1,4 +1,6 @@
 
 capabilities = Selenium::WebDriver::Remote::Capabilities.phantomjs("phantomjs.page.settings.userAgent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
+
 driver = Selenium::WebDriver.for :phantomjs, :desired_capabilities => capabilities
+
 b = ::Watir::Browser.new driver
