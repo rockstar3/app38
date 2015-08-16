64af75ebbaab0f05798da9e426748e2d89611394
root <kolja.schneider1125@gmail.com>
2015-08-16 08:48:06 +0200

    test

--- regex.rb
+++ regex.rb
@@ -1,6 +1,6 @@
 def scrap_link_from_styles(style)
 	image_url = style.match(/background: url\((.*?)\)/)[1]
 end
-def api_resource
-	API_URL.gsub("%%products_per_page%%", "1000").gsub("%%folderId%%", folderId)
-end
+# def api_resource
+# 	API_URL.gsub("%%products_per_page%%", "1000").gsub("%%folderId%%", folderId)
+# end
